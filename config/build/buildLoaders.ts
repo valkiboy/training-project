d8411d52a9ad58a78162e08ecf73c91401c18321
unknown <val_kik@mail.ru>
2023-06-11 14:08:57 +0300

    Добавляет babel и extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,21 +4,40 @@ import { BuildOptions } from './types/config';
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
 
-    const {isDev, paths} = options
+    const { isDev, paths } = options
 
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
-        }
-        
-        const fileLoader = {
-            test: /\.(png|jpe?g|gif)$/i,
-            use: [
-              {
+    }
+
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+            {
                 loader: 'file-loader',
-              },
-            ],
+            },
+        ],
+    }
+
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    ["i18next-extract",
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true
+                        }
+                    ],
+                ]
+            }
         }
+    }
 
     const cssLoader = {
         test: /\.s[ac]ss$/i,
@@ -53,6 +72,7 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader
     ]
